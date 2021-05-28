87774dc7c5d4eb2023823c00a34d2c5deab60de9
khaiphong <khaiphong.prajna@gmail.com>
2021-05-28 05:13:44 -0600

    embedded BadgerDB

--- go/main.go
+++ go/main.go
@@ -9,6 +9,10 @@
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  
+  Mu EIP-Default() initiates EIP Free services to registered user: (1) Free 
+  Chat, (2) Free Todos list, (3) Free accesses to PersonaDB, (4) Free access 
+  to PersonaAI, (5) Free access to KP-Platform 
 */
 
 package main
